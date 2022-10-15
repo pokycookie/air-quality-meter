b6d30a8e58e53c3067e548abca0c5907dc0babf6
PokyCookie <0cookieboy0@gmail.com>
2022-10-15 22:34:01 +0900

    structure

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.json("homepage");
 });
 
+// API Route
+app.post("/api/upload", (req, res) => {
+  // upload data
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
