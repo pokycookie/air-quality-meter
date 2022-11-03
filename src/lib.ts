88b82eda399eea6418f53db01631e6f00ae025e0
PokyCookie <0cookieboy0@gmail.com>
2022-11-04 01:33:43 +0900

    REACT_APP_HOST

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { IData } from "./types";
 
-const URI = process.env.NODE_ENV === "development" ? "http://localhost:4000" : process.env.HOST;
+const URI =
+  process.env.NODE_ENV === "development" ? "http://localhost:4000" : process.env.REACT_APP_HOST;
 
 export function getData() {
   return axios.get<IData[]>(`${URI}/api/data`);
